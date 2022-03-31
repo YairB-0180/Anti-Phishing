b809cc1efaece6bbc9489732023b3b8018c2b624
Selina <l17590580@sjuanrio.tecnm.mx>
2022-03-31 17:59:20 -0600

    Interfaces

---
+++ app/src/main/java/com/example/antiphishing/Seguridad.java
@@ -0,0 +1,14 @@
+package com.example.antiphishing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Seguridad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seguridad);
+    }
+}
